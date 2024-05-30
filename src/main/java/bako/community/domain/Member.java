0e3c36d9f69789df2fbc892d58c6dd759358c962
parkhojeong <parkhj062@gmail.com>
2024-05-31 00:09:42 +0900

    database development setup
    - h2, hibernate

---
+++ src/main/java/bako/community/domain/Member.java
@@ -0,0 +1,11 @@
+package bako.community.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member{
+    @Id @GeneratedValue
+    private Long id;
+}
